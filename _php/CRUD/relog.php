24f0444e3c92706d0f2a20005b147c21d787f6b8
Cristhiandlr <cristhian.limarohca@gmail.com>
2025-02-04 14:23:04 -0300

    Alterando conexão servidor

--- _php/CRUD/relog.php
+++ _php/CRUD/relog.php
@@ -1,7 +1,14 @@
 <?php
-    session_start();
-    $usuario = $_SESSION['usuario'];
-    $senha = $_SESSION['senha'];
+   session_start();
+   if (isset($_SESSION['usuario']) && isset($_SESSION['senha'])) {
+       $usuario = $_SESSION['usuario'];
+       $senha = $_SESSION['senha'];
+   } else {
+       // Se as variáveis não existirem, redirecionar ou informar erro
+       echo "<script>alert('Sessão não encontrada.'); window.location.href = 'login.php';</script>";
+       exit();
+   }
+   
     include("conexao.php");
     $conn = mysqli_criar();
     $sqlBusca = "SELECT * FROM usuarios WHERE usuario = '{$usuario}' AND senha = '{$senha}'";
